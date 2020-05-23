765487035f308f255d19f9fd299d1b6940d6b000
Maria Afanaseva <mashenkachukina@gmail.com>
2020-05-23 14:35:51 +0200

    Added json files, updated models, added fill_db

---
+++ mainapp/management/commands/fill_db.py
@@ -0,0 +1,89 @@
+import json
+import os
+from django.core.management.base import BaseCommand
+from mainapp.models import (Teachers, Addresses, Books,
+                            Courses, LanguageCourses)
+
+
+JSON_PATH = 'mainapp/json'
+
+
+def load_from_json(file_name):
+    with open(os.path.join(JSON_PATH, file_name + '.json'), 'r') as json_file:
+        return json.load(json_file)
+
+
+def save_teachers():
+    teachers = load_from_json('teachers')
+
+    Teachers.objects.all().delete()
+    for teacher in teachers:
+        new_teacher = Teachers(**teacher)
+        new_teacher.save()
+
+
+def save_addresses():
+    addresses = load_from_json('addresses')
+    Addresses.objects.all().delete()
+    for address in addresses:
+        new_address = Addresses(**address)
+        new_address.save()
+
+
+def save_books():
+    books = load_from_json('books')
+    Books.objects.all().delete()
+    for book in books:
+        new_book = Books(**book)
+        new_book.save()
+
+
+def save_courses():
+    courses = load_from_json('courses')
+    Courses.objects.all().delete()
+    for course in courses:
+        street = course["address"]
+        _address = Addresses.objects.get(street=street)
+        course['address'] = _address
+
+        # get all teachers
+        teacher_names = course["teacher"].split(', ')
+        teachers = []
+        for teacher in teacher_names:
+            get_teacher = Teachers.objects.get(name=teacher)
+            teachers.append(get_teacher)
+        course.pop('teacher')
+
+        new_course = Courses(**course)
+        new_course.save()
+        new_course.teacher.add(*teachers)
+
+
+def save_language_courses():
+    courses = load_from_json('language_courses')
+    LanguageCourses.objects.all().delete()
+    for course in courses:
+        course_name = course['course']
+        _get_course = Courses.objects.get(name=course_name)
+        course['course'] = _get_course
+
+        # get all books
+        book_names = course["book"].split(', ')
+        books = []
+        for book in book_names:
+            get_book = Books.objects.get(name=book)
+            books.append(get_book)
+        course.pop('book')
+
+        new_lang_course = LanguageCourses(**course)
+        new_lang_course.save()
+        new_lang_course.book.add(*books)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        save_teachers()
+        save_addresses()
+        save_books()
+        save_courses()
+        save_language_courses()
--- mainapp/models.py
+++ mainapp/models.py
@@ -4,42 +4,60 @@ from django.db import models
 class Teachers(models.Model):
     name = models.CharField(verbose_name='teacher name', max_length=128)
     surname = models.CharField(verbose_name='teacher surname', max_length=128)
-    age = models.PositiveIntegerField(verbose_name='teacher age', null=True)
+    dob = models.DateField(verbose_name='teacher date of birth', null=True)
     education = models.CharField(verbose_name='teacher education', max_length=255, blank=True)
     introduction = models.TextField(verbose_name='teacher introduction text', blank=True)
     photo = models.ImageField(upload_to='teacher photo', blank=True)
 
+    def __str__(self):
+        return f'{self.name}, {self.surname}'
+
 
 class Addresses(models.Model):
-    country = models.CharField(verbose_name='country', max_length=128)
-    city = models.CharField(verbose_name='city', max_length=128)
-    street = models.CharField(verbose_name='street', max_length=255)
-    house_number = models.IntegerField(verbose_name='house number')
-    floor = models.IntegerField(verbose_name='house floor')
-    room_number = models.IntegerField(verbose_name='room number')
+    country = models.CharField(verbose_name='country', max_length=128, blank=True)
+    city = models.CharField(verbose_name='city', max_length=128, blank=True)
+    street = models.CharField(verbose_name='street', max_length=255, blank=True)
+    house_number = models.IntegerField(verbose_name='house number', blank=True, null=True)
+    floor = models.IntegerField(verbose_name='house floor', blank=True, null=True)
+    room_number = models.IntegerField(verbose_name='room number', blank=True, null=True)
+    google_map = models.CharField(verbose_name='google map', max_length=500, blank=True)
+
+    def __str__(self):
+        return f'{self.country}, {self.city}, {self.street}, {self.house_number}'
 
 
 class Courses(models.Model):
     name = models.CharField(verbose_name='course name', max_length=128)
-    short_desc = models.CharField(verbose_name='course short description', max_length=60, blank=True)
+    short_desc = models.CharField(verbose_name='course short description',
+                                  max_length=60, blank=True)
     description = models.TextField(verbose_name='course description')
     price = models.DecimalField(verbose_name='course price', max_digits=8, decimal_places=2, default=0)
-    teacher = models.ManyToManyField(Teachers, verbose_name='teacher')
-    date = models.DateTimeField(verbose_name='course date')
+    teacher = models.ManyToManyField(Teachers, verbose_name='teacher', blank=True)
+    start_date = models.DateTimeField(verbose_name='course start date', null=True)
+    duration = models.PositiveIntegerField(verbose_name='course duration', default=1)
     address = models.ForeignKey(Addresses, models.SET_NULL,
                                 blank=True, null=True,
                                 verbose_name='course address')
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Books(models.Model):
     name = models.CharField(verbose_name='book name', max_length=255)
     author = models.CharField(verbose_name='book author', max_length=128)
-    description = models.TextField(verbose_name='book description')
+    description = models.TextField(verbose_name='book description', blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class LanguageCourses(models.Model):
-    course_id = models.OneToOneField(Courses, primary_key=True, on_delete=models.CASCADE)
+    course = models.OneToOneField(Courses, primary_key=True, on_delete=models.CASCADE)
     language = models.CharField(verbose_name='language', max_length=128)
     level_letter = models.CharField(verbose_name='language level letter', max_length=1)
     level_number = models.FloatField(verbose_name='language level number', max_length=3)
     book = models.ManyToManyField(Books, verbose_name='book')
+
+    def __str__(self):
+        return f'{self.language}'
